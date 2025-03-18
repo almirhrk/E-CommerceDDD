70ebba66f3911be1578f60da5313ce51d86483d5
almir <al.mirrhrk@gmail.com>
2025-03-18 11:29:29 -0300

    Criação contextoBase, clase Generica de produto

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> produto { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data source=DESKTOP-3COH784;database=E_Commerce_DDD;Trusted_connection=true; Encrypt=false; TrustServerCertificate=true";
+            return strCon;
+        }
+    }
+}
---
+++ Infrastructure/Repository/Repositories/RepositoryProduct.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Repositories
+{
+    class RepositoryProduct : RepositoryGenerics<Produto>, IProduct
+    {
+        public Task<List<Produto>> ListarProdutos(Expression<Func<Produto, bool>> exProduto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Produto>> ListarProdutosCarrinhoUsuario(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Produto>> ListarProdutosUsuario(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Produto>> ListarProdutosVendidos(string userId, string filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Produto> ObterProdutoCarrinho(int idProdutoCarrinho)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
