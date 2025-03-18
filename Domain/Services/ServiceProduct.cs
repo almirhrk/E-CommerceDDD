a179083957d037d3646ab7d4ca1e318bd2e59077
almir <al.mirrhrk@gmail.com>
2025-03-18 10:35:34 -0300

    Criação do projeto, pasta, Interface, regra de negocio

---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+
+        private readonly IProduct _IProduct;
+
+        public ServiceProduct(IProduct IProduct)
+        {
+            _IProduct = IProduct;
+        }
+
+        public async Task AddProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+                    if (validaNome && validaValor) 
+            {
+                produto.Estado = true;
+                await _IProduct.Add(produto);
+            }
+        }
+
+        public Task<List<Produto>> ListarProdutosComEstoque(string descricao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                await _IProduct.Update(produto);
+            }
+        }
+    }
+}
+
---
+++ Entities/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+    public class Produto : Notifies
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("PRD_NOME")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("PRD_VALOR")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+        [Column("PRD_ESTADO")]
+        [Display(Name = "Estado")]
+        public bool Estado { get; set; }
+    }
+}
